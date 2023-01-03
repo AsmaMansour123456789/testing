13d265e5449088be91f600e4d27b1611d29b927f
AsmaMansour <86351672+AsmaMansour123456789@users.noreply.github.com>
2023-01-03 08:32:17 +0100

    Update __manifest__.py

--- payroll_Asma_Mansour/__manifest__.py
+++ payroll_Asma_Mansour/__manifest__.py
@@ -18,7 +18,7 @@
     'images': ['static/description/Banner.jpg'],
     "license": "LGPL-3",
     "author": "Asma Mansour",
-    "depends": ["hr_contract", "base", "hr", "hr_holidays", "hr_payroll","hr_work_entry","account_accountant", "l10n_tunisia", ],
+    "depends": ["hr_contract", "base", "hr", "hr_holidays", "hr_payroll","hr_work_entry","account_accountant",],
     "data": [
         "views/hr_employee_views.xml",
         "views/hr_contract_views.xml",
